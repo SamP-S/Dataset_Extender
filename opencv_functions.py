2685a8f9dcce950b62f8cb4b23ecb0cf23973d00
SamP-S <sam.ps1@outlook.com>
2023-11-10 17:56:37 +0000

    Add blend, remove colour, remove range, load directory

--- opencv_functions.py
+++ opencv_functions.py
@@ -84,31 +84,65 @@ def salt_pepper_noise(img, ratio=0.5, freq=0.05, b_w=False):
         coord = [np.random.randint(0, i) for i in img.shape]
         if i <= salt:
             if b_w:
-                noisy[coord[0]][coord[1]] = (255, 255, 255)
+                noisy[coord[0]][coord[1]] = [255 for i in img.shape[2]]
             else:
                 noisy[coord[0]][coord[1]][coord[2]] = 255
         else:
             if b_w:
-                noisy[coord[0]][coord[1]] = (0, 0, 0)
+                noisy[coord[0]][coord[1]] = [0 for i in img.shape[2]]
             else:
                 noisy[coord[0]][coord[1]][coord[2]] = 0
     return noisy
 
-# Mix img and img2 using ratio
-def mix(img, img2, ratio=0.2):
+# Mix images using linear ratio
+def mix_images(img, img2, ratio=0.2):
     return (img * ratio).astype(np.uint8) + (img2 * (1 - ratio)).astype(np.uint8)
 
+# Insert image at coordinates (no mixing/blending)
 def insert_image(img, img2, x, y):
     result = np.copy(img)
+    print(img2.shape)
     result[y: y + img2.shape[1], x: x + img2.shape[0]] = img2
     return result
+
+# Blend image into another using transparency 
+### TODO slow
+def blend_images(img, img2, x, y):
+    result = np.copy(img)
+    for j in range(img2.shape[1]):
+        for i in range(img2.shape[0]):
+            ### TODO use transparency as proportionate ratio
+            if img2[j, i, 3] != 0:
+                result[j + y, i + x] = img2[j, i]
+    return result
+
+# Set specific colour to transparent
+def remove_colour(img, r, g, b, a):
+    result = np.copy(img)
+    cells = np.where(np.all(img == [b, g, r, a], axis=-1))
+    result[cells[0], cells[1], :] = [b, g, r, 0]
+    return result
+
+# Set specific colour range to transparent
+def remove_colour_range(img, c1, c2):
+    mask = cv2.inRange(img, c1, c2) # create the Mask
+    mask = 255 - mask  # inverse mask
+    return cv2.bitwise_and(img, img, mask=mask)
 
 # Load image from file
 # cv2.IMREAD_COLOR
 # cv2.IMREAD_GRAYSCALE 
 # cv2.IMREAD_UNCHANGED
 def load_image(path, colourspace=cv2.IMREAD_COLOR):
-    return cv2.imread(path, colourspace)
+    img = cv2.imread(path, colourspace)
+    return convert_colourspace(img)
+
+def load_dir(path):
+    imgs = []
+    for filename in os.listdir(path):
+        img_path = os.path.join(path, filename)
+        imgs.append(load_image(img_path))
+    return imgs
 
 # NOTE: images are BGR
 def display_image(img):
@@ -117,7 +151,7 @@ def display_image(img):
 
 # cv2.COLOR_BGR2RGB
 # cv2.COLOR_RGB2BGR
-def convert_colourspace(img, conversion=cv2.COLOR_RGB2BGR):
+def convert_colourspace(img, conversion=cv2.COLOR_RGB2BGRA):
     return cv2.cvtColor(img, conversion)
 
 if __name__ == "__main__":
@@ -125,22 +159,33 @@ if __name__ == "__main__":
     bricks_dir = os.path.join(cwd, "bricks")
     bg_dir = os.path.join(cwd, "backgrounds")
 
-    test_img_path = os.path.join(bricks_dir, "3023_0.png")
-    test_img2_path = os.path.join(bricks_dir, "4073_0.png")
-    test_img = load_image(test_img_path)
-    test_img2 = load_image(test_img2_path)
-    test_img = convert_colourspace(test_img)
-    test_img2 = convert_colourspace(test_img2)
+    brick_imgs = load_dir(bricks_dir)
+    bg_imgs = load_dir(bg_dir)
 
-    # display_image(test_img)
-    # display_image(test_img2)
-    # display_image(rotate(test_img, cv2.ROTATE_90_CLOCKWISE))
-    # display_image(uniform_scale(test_img, 2, cv2.INTER_NEAREST))
-    # display_image(scale(test_img, 2, 0.5, cv2.INTER_NEAREST))
-    # display_image(guassian_noise(test_img, 2))
+    test_img = brick_imgs[0]
+    test_img2 = brick_imgs[1]
+
+    display_image(test_img)
+    display_image(test_img2)
+
+    display_image(rotate(test_img, cv2.ROTATE_90_CLOCKWISE))
+    display_image(uniform_scale(test_img, 2, cv2.INTER_NEAREST))
+    display_image(scale(test_img, 2, 0.5, cv2.INTER_NEAREST))
+
+    display_image(guassian_noise(test_img, 2))
     # display_image(salt_pepper_noise(test_img))
     # display_image(salt_pepper_noise(test_img, b_w=True))
-    # display_image(mix(test_img, test_img2))
+
+    display_image(mix_images(test_img, test_img2))
     scaled_test2_img = scale(test_img2, 0.5, 0.5)
     display_image(insert_image(test_img, scaled_test2_img, 100, 100))
+
+    colour_histogram(test_img)
+
+    bg_remove = remove_colour(test_img, 70, 70, 70, 255)
+    bg_remove = remove_colour(bg_remove, 71, 71, 71, 255)
+    display_image(bg_remove)
+
+    bg_rem = remove_colour_range(test_img, (70, 70, 70, 255), (72, 72, 72, 255))
+    display_image(blend_images(bg_imgs[0], bg_rem, 100, 100))
 
