667926accd3d21d29e7c2b5150c380360259f2b1
SamP-S <sam.ps1@outlook.com>
2023-12-22 18:24:44 +0000

    Update install_requirements and license

--- setup.py
+++ setup.py
@@ -1,25 +1,25 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='cve',
+    name='ocve',
     version='0.1.0',
     packages=find_packages(),
     install_requires=[
-        "numpy",
-        "opencv",
-        "matplotlib"
+        'numpy',
+        'opencv-python',
+        'matplotlib'
     ],
     author='Sam Pearson-Smith',
     author_email='samps1@outlook.com',
     description='Extension of OpenCV4',
     long_description='Adds more functionality, input validation for safer usage, extended with more features',
     long_description_content_type='text/markdown',
-    url='https://github.com/SamP-S/OpenCV_Extended',  # URL to the package repository
-    license='GPL-3.0',  # Specify the license type
+    url='https://github.com/SamP-S/OpenCV_Extended',
+    license='Apache License, Version 2.0',
     classifiers=[
         'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: GPL Liscense',
+        'License :: OSI Approved :: Apache Software License',
         'Operating System :: OS Independent',
         'OpenCV :: Computer Vision'
-    ],
+    ]
 )
